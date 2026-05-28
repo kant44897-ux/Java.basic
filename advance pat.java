b7d2ed219b04e5741c4258896ea843789434a2fb
kant44897-ux <kant44897@gmail.com>
2026-05-28 20:55:05 +0530

    Update advance pat.java

--- advance pat.java
+++ advance pat.java
@@ -1,5 +1,14 @@
 import java.util.Scanner;
 class Main{
+
+public static void inVr( int n ){
+    for( int i = 1; i <= n; i++){
+        for( int j = 1; j <= n-i+1; j++){
+            System.out.print("* ");
+        }
+          System.out.println();
+    }
+}
 public static void butFly( int n ){
     for( int i = 1; i <= n; i++){
         for( int j = 1; j<=i; j++){
