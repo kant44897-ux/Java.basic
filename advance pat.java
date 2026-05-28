88355a923157c2a8421ef062f8068ccfcf0011b4
kant44897-ux <kant44897@gmail.com>
2026-05-29 00:12:47 +0530

    Update advance pat.java

--- advance pat.java
+++ advance pat.java
@@ -1,6 +1,72 @@
 import java.util.Scanner;
 class Main{
-
+    
+public static void payRa( int n ){
+    for( int i = 1; i<= n; i++){
+        for( int j = 1; j <= n-i; j++){
+            System.out.print("  ");
+        }
+        for( int j = 1; j <= 2*i-1; j++){
+            System.out.print("* ");
+        }
+              System.out.println(); 
+        }
+        for( int i = n-1; i >= 1; i--){
+             for( int j = 1; j <= n-i; j++){
+                 System.out.print("  ");
+             }
+             for( int j = 1; j<=2*i-1; j++){
+                 System.out.print("* ");
+             }
+             System.out.println();
+        }
+    }    
+public static void ultapyaa( int n ){
+    int num = n;
+    for( int i = 1; i <= n; i++){
+        for( int j = 1; j <= n-i; j++){
+          System.out.print(" ");
+        }
+        for( int j = 1; j<=i; j++){
+             System.out.print(" " +num);
+             num--;
+        }
+         System.out.println();
+    }
+}    
+public static void ultapya( int n ){
+    for( int i = 1; i <= n; i++){
+        for( int j = 1; j <= i-1; j++){
+             System.out.print(" ");
+        }
+        for( int j = 1; j <= n-i+1; j++){
+             System.out.print(" " +j);
+        }
+        System.out.println();
+    }
+}
+public static void backVrr( int n ){
+    for( int i = 1; i <= n; i++){
+        for( int j = 1; j <= n-i; j++){
+              System.out.print(" ");
+        }
+        for( int j = 1; j <= i; j++){
+              System.out.print(" " +j);
+        }
+        System.out.println();
+    }
+}
+public static void backVr( int n ){
+    for( int i = 1; i <= n; i++){
+        for( int j = 1; j<=i-1;j++){
+             System.out.print("  ");
+        }
+        for( int j = 1; j<=n-i+1; j++){
+            System.out.print("* ");
+        }
+         System.out.println();
+    }
+}
 public static void inVr( int n ){
     for( int i = 1; i <= n; i++){
         for( int j = 1; j <= n-i+1; j++){
