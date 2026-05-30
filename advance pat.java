713567727cf9734e7d62b5825c36d0b681b633bd
kant44897-ux <kant44897@gmail.com>
2026-05-30 17:27:42 +0530

    Update advance pat.java

--- advance pat.java
+++ advance pat.java
@@ -1,6 +1,22 @@
 import java.util.Scanner;
 class Main{
     
+ public static void mirrPat( int n ){
+        for( int i = 1; i <= n; i++){
+            for( int j = 1; j <= i; j++){
+                  System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+         
+    for( int i = n-1; i >=1; i--){
+        for( int j = 1; j <= i; j++){
+             System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+}
+    
 public static void payRa( int n ){
     for( int i = 1; i<= n; i++){
         for( int j = 1; j <= n-i; j++){
