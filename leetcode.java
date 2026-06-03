e56a5a357c68387e873ad25ed59b4f92d477b0f7
kant44897-ux <kant44897@gmail.com>
2026-06-04 01:12:50 +0530

    Update leetcode.java

--- leetcode.java
+++ leetcode.java
@@ -1,4 +1,32 @@
-public int maxArea(int[] height) {
+33. public int search(int[] nums, int target) {
+        int low = 0; 
+        int high = nums.length - 1;
+
+        while( low <= high){
+            
+            int mid = (low + high)/2;
+            if( nums[mid] == target){
+                return mid;
+            }
+            if(nums[mid] >= nums[low]){
+                if( target >= nums[low] && target < nums[mid]){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+              } else{
+             if( target > nums[mid] && target <= nums[high]){
+                low = mid + 1;
+                 }else {
+                high = mid - 1;
+                 }
+            }
+        }
+        return -1;
+    }
+}
+
+11. public int maxArea(int[] height) {
         int i = 0;
         int j = height.length - 1;
         int maxArea = 0;
@@ -18,7 +46,7 @@ public int maxArea(int[] height) {
         }
         
 }
-public int[] twoSum(int[] nums, int target) {
+1. public int[] twoSum(int[] nums, int target) {
         for( int i = 0; i < nums.length; i++){
             for( int j = i + 1; j < nums.length; j++){
                 int currSum = nums[i] + nums[j];
