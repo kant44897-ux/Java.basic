7cb2ac6f325ac26359d0da8e0cf4ec0a63c7dd55
kant44897-ux <kant44897@gmail.com>
2026-06-03 14:17:15 +0530

    Implement maxArea function for container problem

--- leetcode.java
+++ leetcode.java
@@ -1,3 +1,23 @@
+public int maxArea(int[] height) {
+        int i = 0;
+        int j = height.length - 1;
+        int maxArea = 0;
+
+        while(i < j){
+            int b = j - i;
+            int h = Math.min(height[i] , height[j]);
+            int area = h * b;
+            maxArea = Math.max(maxArea , area);
+            if(height[i] < height[j]){
+                i++;
+            }else {
+                j--;
+            }
+        }
+            return maxArea;
+        }
+        
+}
 public int[] twoSum(int[] nums, int target) {
         for( int i = 0; i < nums.length; i++){
             for( int j = i + 1; j < nums.length; j++){
